f05475b631fac13c6bdeb46866d3bfa935b9d26d
Andreas-ita <andreas.itana@outlook.com>
2025-03-24 16:31:34 +0200

    forms

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -27,7 +27,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
             </section>
             <section class="listing-apply">
                 <h2 class="section-heading">Apply now to live here</h2>
-                <form [formGroup]="applyForm" (submit)="submitApplication">
+                <form [formGroup]="applyForm" (submit)="submitApplication()">
                     <label for="first-name">First Name</label>
                     <input id="first-name" type="text" formControlName="firstName">
 
@@ -55,9 +55,11 @@ export class DetailsComponent {
 
     constructor() {
         const housingLocationId = Number(this.route.snapshot.params['id']);
-        this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+        this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+            this.housingLocation = housingLocation;
+        });
 
-        submitApplication(); {
+        submitApplication() {
             this.housingService.submitApplication(
                 this.applyForm.value.firstName ?? '',
                 this.applyForm.value.lastName ?? '',
