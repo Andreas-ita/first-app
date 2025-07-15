a8499c561cdebc0ead6947089a998b568c39b360
Andreas-ita <andreas.itana@outlook.com>
2025-07-15 14:45:28 +0200

    carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
@@ -20,11 +20,26 @@ export class HomeComponent {
     housingService: HousingService = inject(HousingService);
     filteredLocationList: HousingLocation[] = [];
 
+    //carousel item data
+    carouselItems = ['Soccer', 'Aviator', 'Dino', 'Jackpot', 'Roulette', 'My Account'];
+
     constructor() {
         this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
             this.housingLocationList = housingLocationList;
             this.filteredLocationList = housingLocationList;
         });
+    }
+
+    @ViewChild('carousel', { static: false }) carousel!: ElementRef;
+
+    scrollCarousel(direction: 'left' | 'right') {
+        const scrollAmount = 200;
+        const el = this.carousel.nativeElement;
+        if (direction === 'left') {
+            el.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+        } else {
+            el.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+        }
     }
 
     filterResults(text: string) {
